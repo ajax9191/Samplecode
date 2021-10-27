c3e011e105d69d0382b1b4963f57b3e36e3b10d2
ajax9191 <93190881+ajax9191@users.noreply.github.com>
2021-10-27 14:10:02 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,12 @@
+public class SumOfNumbers
+{
+    
+    public static void main(String args[])
+    {  
+
+        int n3 = 69, n4 = 50, addition;
+        sum = n4 + n3;
+        System.out.println("The sum of numbers : " +addition);
+
+    }
+}
