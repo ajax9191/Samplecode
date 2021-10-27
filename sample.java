39dc30052c24142fa8b376451c54eca4c53d9a2f
ajax9191 <93190881+ajax9191@users.noreply.github.com>
2021-10-27 14:11:34 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -4,8 +4,8 @@ public class SumOfNumbers
     public static void main(String args[])
     {  
 
-        int n3 = 69, n4 = 50, addition;
-        sum = n4 + n3;
+        int n1 = 69, n2 = 50, addition;
+        sum = n1 + n2;
         System.out.println("The sum of numbers : " +addition);
 
     }
